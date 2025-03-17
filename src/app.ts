e499c5f6e6542f296f37af26bbc81c15aa3ef44e
Sebastião Sukuakueche <sukuakueches@gmail.com>
2025-03-17 09:29:13 +0000

    add limiter for request from one ip
    Signed-off-by: Sebastião Sukuakueche <sukuakueches@gmail.com>

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,9 @@ import express from 'express';
 import { envConfig } from './config/env';
 import { initRoutes } from './routes/app.route';
 import cors from 'cors';
+import rateLimit from 'express-rate-limit';
+
+
 
 //instanciar o express
 const app = express();
@@ -12,11 +15,20 @@ app.use(express.json())
 //habilitar cors
 app.use(cors());
 
+//limitar requisições de um usuário para evitar DDos
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutos
+    max: 100, // Máximo de 100 requisições por IP
+    message: "Muitas requisições. Tente novamente mais tarde."
+});
+
+app.use(limiter);
+
 //inicializar as rotas
 initRoutes(app);
 
 
 // iniciar a escuta na porta especifícada
-app.listen(envConfig.PORT, ()=>{
+app.listen(envConfig.PORT, () => {
     console.log(`Servidor rodando na porta ${envConfig.PORT}`)
 })
